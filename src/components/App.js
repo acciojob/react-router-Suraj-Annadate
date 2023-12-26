000da40c9f7bc8384aa578fd2d7af67b621296ac
suraj0698 <annadatesuraj@gmail.com>
2023-12-26 12:08:25 +0530

    c1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import About from "./pages/About";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
